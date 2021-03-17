6d7bbf7224a7787c664ca7c4ddd200b48a569d07
Robert Joseph Cox Jr <robert@studiorclv.com>
2021-03-16 22:03:57 -0700

    shifted the setup up

--- fedora/setup.sh
+++ fedora/setup.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-# Source dependencies
-source ./utilities/detection.sh
-source ./utilities/output.sh
-
-# Check the distro to make sure we're executing the right provisioner
-DISTRO=$(detection.getDistro)
-
-if [[ "${DISTRO}" == "Fedora" ]]; then
-  output.echoIndentedLine "You are trying to run wrong provisioner"
-  output.echoIndentedLine "Run the provisioner for '${DISTRO}' instead"
-
-  exit 1
-fi
-
 # source installers
 source ./fedora/installers/dnf.sh
 source ./fedora/installers/flatpak.sh
--- start.sh
+++ start.sh
@@ -1,16 +1,37 @@
 #!/bin/bash
 
-git clone https://github.com/Robert430404/provisioners.git
+REPO_NAME="provisioners"
 
-cd ./provisioners || exit 1
+# Sets up the script
+function start.setup {
+  git clone "https://github.com/Robert430404/${REPO_NAME}.git"
 
-echo "What distribution are you provisioning? [fedora]"
-read -r DISTRO
+  cd "./${REPO_NAME}" || exit 1
+}
 
-if [[ "${DISTRO}" == "fedora" ]]; then
+# Cleanup function
+function start.cleanup {
+  cd ../ || exit 1
+
+  rm -Rf "./${REPO_NAME}"
+}
+
+start.setup
+
+# Source dependencies
+source ./utilities/detection.sh
+source ./utilities/output.sh
+
+# Check the distro to make sure we're executing the right provisioner
+DISTRO=$(detection.getDistro)
+
+if [[ "${DISTRO}" == "Fedora" ]]; then
   source ./fedora/setup.sh
+
+  start.cleanup
+else
+  output.echoIndentedLine "no valid provisioner found"
+  start.cleanup
+
+  exit 1
 fi
-
-cd ../ || exit 1
-
-rm -Rf ./provisioners
